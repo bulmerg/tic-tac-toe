c9545f282f68f9c0c36ab291124eea0849621f7e
Glen Bulmer <glenbulmer@Glens-MacBook-Pro.local>
2020-10-19 16:07:32 -0400

    pass player1 name test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Player } from './models/Player';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe';
+  player1: Player;
+
+  constructor () {
+    this.player1 = new Player();
+  }
 }
