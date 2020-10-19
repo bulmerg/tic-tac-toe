b4df70deb5fefc5af09beea8f330e1a6c4ddc959
Glen Bulmer <glenbulmer@Glens-MacBook-Pro.local>
2020-10-19 16:21:31 -0400

    Fixed  player2 test for name and value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,10 @@ import { Player } from './models/Player';
 export class AppComponent {
   title = 'tic-tac-toe';
   player1: Player;
+  player2: Player;
 
   constructor () {
     this.player1 = new Player();
+    this.player2 = new Player();
   }
 }
